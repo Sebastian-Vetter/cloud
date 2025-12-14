a20a259509f4f162de5e940b86504a628063e282
MrSpigot <220609166+Sebastian-Vetter@users.noreply.github.com>
2025-12-14 01:36:58 +0100

    output-screen(Added screen interface)

---
+++ core-app/src/main/kotlin/de/aptcode/minecraft/cloud/core/cli/service/OutputMonitorAdapter.kt
@@ -0,0 +1,22 @@
+/**
+ * @author Sebastian Vetter
+ * @company aptcode.de
+ * @date 14.12.25
+ * @project cloud
+ *
+ * Copyright (c) 2025 aptcode.de. All rights reserved.
+ */
+
+package de.aptcode.minecraft.cloud.core.cli.service
+
+import de.aptcode.minecraft.cloud.core.infrastructure.cli.service.OutputMonitorService
+
+class OutputMonitorAdapter: OutputMonitorService{
+    override fun startMonitoring() {
+        TODO("Not yet implemented")
+    }
+
+    override fun stopMonitoring() {
+        TODO("Not yet implemented")
+    }
+}
